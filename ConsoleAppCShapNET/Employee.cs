6d7b547c36bd92e1bfefaefa0ebaf8b9d4a61165
buihuudong19 <buihuudong19@gmail.com>
2022-10-22 09:12:43 +0700

    Add project files.

---
+++ ConsoleAppCShapNET/Employee.cs
@@ -0,0 +1,72 @@
+﻿namespace ConsoleAppCShapNET.Entities;
+public class Employee
+{
+    //1. fields (data)
+    private int _id;
+    private string _name;
+    private DateOnly _dob;
+    //2. properties
+    public int Id { 
+        get { return _id; } 
+        set { _id = value; } 
+    }
+    private static int _lastID = 0; //Id cuoi cung cua employee
+    public static int NextId = ++_lastID;
+    public string Name { get => _name; set => _name = value; }
+    public DateOnly Dob { get => _dob; set => _dob = value; }
+
+    public string Address { get; set; }
+    public int WorkHourly { get; set; }
+    public float WorkRate { get; set; }
+
+    public static Employee[] Employees { get; }
+
+
+    
+    
+    
+    //3. constructors
+    //overloading...
+    static Employee()
+    {
+        Employees = new Employee[2];
+
+        Employee e = new Employee();
+        e.Id = NextId;
+        e.Name = "Nguyen van A";
+        e.Address = "Quan 1, TPHCM";
+        e.Dob = new DateOnly(1987,02,18);
+        e.WorkHourly = 20;
+        e.WorkRate = 300000;
+        
+        Employees[0] = e;
+        Employees[1] = new Employee(NextId, "Long Nguy", new DateOnly(1987, 01, 05), "Quan 3, TPHCM", 25, 400000);
+
+    }
+    public Employee()
+    {
+    }
+    public Employee(int? id = null)
+    {
+        Id = id ?? default;//supported by c#7.1 id = 0=> id = default(int)
+    }
+    public Employee(int id, string name, DateOnly dob, string address, int workHourly, float workRate)
+    {
+        Id = id;
+        Name = name;
+        Dob = dob;
+        Address = address;
+        WorkHourly = workHourly;
+        WorkRate = workRate;
+    }
+
+
+
+    //4. methods vs functions vs behavior
+    public string Show() => _display();
+    private string _display() => $"Id: {Id}, Name: {_name}, Address: {Address}, WorkHourly: {WorkHourly}, WorkRate:{WorkRate}";
+    public double GetSalary() => _calculatorSalary();
+    private double _calculatorSalary() => WorkRate* WorkHourly;
+
+    public override string? ToString() => this._display();
+}
---
+++ ConsoleAppCShapNET/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleAppCShapNET.Entities;
+namespace ConsoleAppCShapNET.App;
+public class Program
+{
+    public static void Main()
+    {
+        //create a object Employee
+        Employee e = new Employee();//e: object/instance
+        e.Id = 1;
+        e.Name = "Bui Huu Dong";
+        e.Address = "Quan 10, TPHCM";
+        e.Dob = new DateOnly(2022, 10, 20);
+        e.WorkHourly = 150;
+        e.WorkRate = 250_000;
+
+        Employee e2 = new Employee(2, "Tri", new DateOnly(1983, 01, 15), "Quan 1", 230, 180_000);
+        e2.Name = "Nguy Long";
+
+        Employee e3 = new Employee(null);
+        int? a = null;
+       
+        if(a == null)
+        {
+            a = 0;
+        }
+        a= a ?? default;
+        Console.WriteLine(a);
+        Console.WriteLine(e2);
+        Console.ReadLine();
+    }
+}
