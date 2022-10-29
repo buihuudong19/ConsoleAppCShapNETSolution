eeff0278b0181cbb720d0bc06f3ad0b9a9bc07e8
buihuudong19 <buihuudong19@gmail.com>
2022-10-29 10:20:06 +0700

    commit

---
+++ OOP.DelegatePro/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Programmming;
+
+
+public class Program
+{
+    public delegate int AddDele(int a, int b);//dinh nghia ra 1 kieu (manual)
+    public delegate void Print(string s);   
+    public static void Main()
+    {
+        //AddDele addDele = new AddDele(Add);
+        AddDele addDele = Add;
+
+        //Truyen ham thong quan lambda expression method
+
+        AddDele addNewDele = (a, b) => a + b;
+        Func<int,int, int> addDeleFunc = (a, b) => a + b;
+        Action<string> printAction = s => Console.WriteLine(s);
+
+        int result = addDele(2, 3);
+        int r = addDele.Invoke(2, 3);
+        Console.WriteLine(r);
+
+        Console.WriteLine("=========================");
+        Display(Add,10,20);
+        Console.ReadLine(); 
+    }
+
+    public static void Display(AddDele dele, int a, int b)
+    {
+        Console.WriteLine($"Tong hai so a va b la: {dele(a, b)}");    
+    }
+
+    public static int Add(int a,int b)
+    {
+        return a + b;
+    }
+
+
+}
---
+++ OOP.LinQ/Program.cs
@@ -0,0 +1,32 @@
+﻿
+string[] lstNames = { "Dong", "Phu", "Linh", "Gia Tri", "Cong Cong" };
+/*Query Expression*/
+/*Sap xep tang dan theo ten va in ra*/
+foreach (string e in lstNames.OrderBy(s => s)){
+    Console.WriteLine(e);   
+}
+/*Sap xep giam dan theo length*/
+foreach (string e in lstNames.OrderBy(a => a.Length))
+{
+    Console.WriteLine(e);
+}
+/*lay ra all names contains character 'n'*/
+
+dynamic data = lstNames.Select(s => s.Contains('n'));
+data = lstNames.Where(s => s.Contains('n'));
+Console.WriteLine("==============================");
+foreach (string e in data)
+{
+    Console.WriteLine(e);
+}
+
+/*Linq to objects*/
+
+var names = from w in lstNames
+            where (w.Contains('n') && w.Length>4)
+            select w;
+Console.WriteLine("==============================");
+foreach (string e in names)
+{
+    Console.WriteLine(e);
+}
